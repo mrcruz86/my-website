cc3c0c782230afb98eacda1c00077ab10620db36
Jason Cruz <mrcruz86@gmail.com>
2014-08-28 22:48:06 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+app.listen(8000);
