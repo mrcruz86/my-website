8c1f1ea88c80c76b039715bb85f60bad153892ef
Jason Cruz <mrcruz86@gmail.com>
2014-09-03 01:57:55 -0700

    added some styles and foundation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/public/views');
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/public'));
